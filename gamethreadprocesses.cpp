49a0d131073560f5808fc2431217fcbcc950a753
N0th1ng18 <nickfre3@gmail.com>
2019-04-24 02:10:49 -0500

    blank commit

--- gamethreadprocesses.cpp
+++ gamethreadprocesses.cpp
@@ -10,12 +10,15 @@ gameThreadProcesses::~gameThreadProcesses()
 
 }
 
+
 void gameThreadProcesses::setup()
 {
     socket = new QUdpSocket(this);
     socket->bind(QHostAddress::AnyIPv4, 2468);
 
     connect(socket, SIGNAL(socket.readyRead()), this, SLOT(readMessageBuffer()));
+
+
 }
 
 void gameThreadProcesses::readMessageBuffer()
